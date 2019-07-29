7f59c0e36417d28b065738454f4d7df2a771dbd0
Code Hunter <mahmood.hafiz@gmail.com>
2019-07-28 22:23:26 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -191,9 +191,9 @@ createRestaurantHTML = (restaurant) => {
  if ('serviceWorker' in navigator) {
   navigator.serviceWorker
   .register('/sw.js')
-  // .then(function(registration) {
-  //   console.log('Registration successful, scope is:', registration.scope);
-  // })
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
   .catch(function(error) {
     console.log("Service worker registration failed, error:", error);
   });
