6bf14c23712b00e2a2a5a229d0bce028d884a5b6
Code Hunter <mahmood.hafiz@gmail.com>
2019-07-28 22:25:24 -0500

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -169,6 +169,19 @@ createReviewHTML = (review) => {
 }
 
 /**
+ * Register Service Worker.
+ */
+ if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log("Service worker registration failed, error:", error);
+  });
+}
+/**
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
